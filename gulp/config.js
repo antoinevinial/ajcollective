87b8ce5bff811516fec3c76828ab43f940c3b451
Antoine Vinial <antoine.vinial@gmail.com>
2015-10-26 21:52:24 +0100

    First commit.

---
+++ gulp/config.js
@@ -0,0 +1,11 @@
+var paths = {
+    base: '../',
+    scripts: 'js/',
+    images: 'images/',
+    templates: 'templates/',
+    css: 'css/',
+    maps: 'maps',
+    dist: '../'
+}
+
+module.exports = paths;
---
+++ gulp/js/main/base.js
@@ -0,0 +1,10 @@
+//import modules
+var moduleExemple = require('./modules/module.js');
+
+(function ($, win) {
+
+    $(function () {
+        moduleExemple();
+    });
+
+})(jQuery, window);
