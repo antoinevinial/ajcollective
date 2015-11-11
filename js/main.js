824b9cc93bea4d3645d0278f12de02265177e054
Antoine Vinial <antoine.vinial@gmail.com>
2015-11-11 18:21:33 +0100

    Adding hover effect inside navigation.

--- js/main.js
+++ js/main.js
@@ -88,11 +88,51 @@ var nav = {
 		this.ui.$nav    = $('.js-nav');
 		this.ui.$toggle = $('.js-nav-toggle');
 		this.ui.$links  = $('.js-nav-link');
+		this.ui.$imgs   = $('.js-nav-img');
+		this.ui.$panels = $('.js-panel');
 	},
 
 	bindEvents: function bindEvents() {
+		var self = this;
+
 		this.ui.$toggle.on('click', $.proxy(this.toggleNav, this));
 		this.ui.$links.on('click', $.proxy(this.getClickedLink, this));
+		this.ui.$panels.on('mouseenter', $.proxy(this.hideImgs, this));
+		this.ui.$toggle.on('mouseenter', $.proxy(this.hideImgs, this));
+
+		this.ui.$links.on('mousemove', self.throttle(function(e){
+		    self.overHandler(e);
+		}, 20));
+	},
+
+	overHandler: function overHandler(e) {
+		var $el  = $(e.currentTarget),
+			$img = $el.find('.js-nav-img');
+			X    = e.pageX,
+			Y    = e.pageY - $el.offset().top;
+
+		// Remove is-hover class on all links.
+		this.ui.$links.not($el).removeClass('is-hover');
+
+		// Translate Image.
+		$img.css({
+		    "-webkit-transform":"translate(" + X + "px," + Y + "px)",
+		    "-moz-transform":"translate(" + X + "px," + Y + "px)",
+		    "-ms-transform":"translate(" + X + "px," + Y + "px)",
+		    "-o-transform":"translate(" + X + "px," + Y + "px)",
+		    "transform":"translate(" + X + "px," + Y + "px)"
+		});
+
+		// Add class is-hover.
+		if (!$el.hasClass('is-hover')) {
+			setTimeout(function() {
+				$el.addClass('is-hover');
+			}, 80);
+		}
+	},
+
+	hideImgs: function hideImgs() {
+		this.ui.$links.removeClass('is-hover');
 	},
 
 	toggleNav: function toggleNav(e) {
@@ -101,6 +141,9 @@ var nav = {
 
 		// Add is-nav-open class on body.
 		this.ui.$body.toggleClass('is-nav-open');
+
+		// Remove is-hover class on all links.
+		this.ui.$links.removeClass('is-hover');
 	},
 
 	getClickedLink: function getClickedLink(e) {
@@ -113,6 +156,28 @@ var nav = {
 		// Add is-active class.
 		this.ui.$links.removeClass('is-active');
 		$(e.currentTarget).addClass('is-active');
+	},
+
+	throttle: function throttle(callback, delay) {
+	    var last;
+	    var timer;
+
+	    return function () {
+	        var context = this;
+	        var now = +new Date();
+	        var args = arguments;
+	        if (last && now < last + delay) {
+	            // le délai n'est pas écoulé on reset le timer
+	            clearTimeout(timer);
+	            timer = setTimeout(function () {
+	                last = now;
+	                callback.apply(context, args);
+	            }, delay);
+	        } else {
+	            last = now;
+	            callback.apply(context, args);
+	        }
+	    };
 	}
 
 };
--- partials/_nav.php
+++ partials/_nav.php
@@ -4,21 +4,21 @@
 			<a class="nav__link js-nav-link" href="#">
 				<span class="nav__link__name">AJ<br>Colomb</span>
 				<span class="nav__link__jobtitle">Creative researcher &amp; founder</span>
-				<img class="nav__link__img" src="http://placehold.it/160x100/"/>
+				<img class="nav__link__img js-nav-img" src="http://placehold.it/160x100/"/>
 			</a>
 		</li>
 		<li class="nav__item">
 			<a class="nav__link js-nav-link" href="#">
 				<span class="nav__link__name">Tegan<br>+ Jody</span>
 				<span class="nav__link__jobtitle">Creative researcher</span>
-				<img class="nav__link__img" src="http://placehold.it/160x100/"/>
+				<img class="nav__link__img js-nav-img" src="http://placehold.it/160x100/"/>
 			</a>
 		</li>
 		<li class="nav__item">
 			<a class="nav__link js-nav-link" href="#">
 				<span class="nav__link__name">Micheal<br>McCool</span>
 				<span class="nav__link__jobtitle">Creative researcher</span>
-				<img class="nav__link__img" src="http://placehold.it/160x100/"/>
+				<img class="nav__link__img js-nav-img" src="http://placehold.it/160x100/"/>
 			</a>
 		</li>
 	</ul>
