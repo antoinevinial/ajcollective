37a8015f7b2494f4c110d2ae7ca6f192384cb0d9
Antoine Vinial <antoine.vinial@gmail.com>
2015-12-02 23:42:56 +0100

    Adding real data for artists name and description.

--- partials/_contact.php
+++ partials/_contact.php
@@ -4,9 +4,9 @@
 		<div class="grid">
 			<div class="cell-12 prepend-5">
 				<h2 class="heading-2 trailer-25">About</h2>
-				<p class="contact__txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-				<p class="contact__txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
-				<p class="contact__txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+				<p class="contact__txt">AJCollective is a multi-disciplined creative team of highly talented individuals who specialise primarily in crafting treatments for some of the most prestigious production companies and Directors in the industry.</p>
+				<p class="contact__txt">Originally founded in 2012 by South African-Londoner-Danish imposter Andrea Jade Colomb, as a creative research company,  it has organically grown and expanded and can now boost not only an arsenal of imagery accumulated over the years, but now also a stellar breed of treatment writers, illustrators and music technicians. All of whom are armed with an encyclopaedic knowledge of films, advertising, design, photography, and visual fancies, along with a solid knowledge of production.</p>
+				<p class="contact__txt">With librarians brains and artists hands, AJCollective’s talent is putting together, thoughtfully and skilfully,  the core idea of the Director’s head, and then some.</p>
 			</div>
 			<div class="cell-12 prepend-2">
 				<h2 class="heading-2 trailer-25">Contact</h2>
