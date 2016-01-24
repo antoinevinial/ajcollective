3ff40672a0b4f0df6fce44180ae56cb4052ae2ab
Antoine Vinial <antoine.vinial@gmail.com>
2016-01-24 14:05:43 +0100

    Save before breaking everything like a boss.

--- partials/_contact.php
+++ partials/_contact.php
@@ -1,29 +1,43 @@
 <div class="contact">
-	<div class="panel">
+	<div class="panel panel--contact">
 		<a class="contact__close js-contact-toggle" href="#">Close<span class="contact__close__icon"><?php include "icons/close.php" ?></span></a>
 		<div class="grid">
-			<div class="cell-11 prepend-4">
+			<div class="prepend-4">
 				<h2 class="heading-2 trailer-25">About</h2>
-				<p class="contact__txt">AJCollective is a multi-disciplined creative team of highly talented individuals who specialise primarily in crafting treatments for some of the most prestigious production companies and Directors in the industry.</p>
-				<p class="contact__txt">Originally founded in 2012 by South African-Londoner-Danish imposter Andrea Jade Colomb, as a creative research company,  it has organically grown and expanded and can now boost not only an arsenal of imagery accumulated over the years, but now also a stellar breed of treatment writers, illustrators and music technicians. All of whom are armed with an encyclopaedic knowledge of films, advertising, design, photography, and visual fancies, along with a solid knowledge of production.</p>
-				<p class="contact__txt">With librarians brains and artists hands, AJCollective’s talent is putting together, thoughtfully and skilfully,  the core idea of the Director’s head, and then some.</p>
+			</div>
+			<div class="cell-11 prepend-4">
+				<p class="contact__txt">Once upon a time, South African/Londoner  and now Danish imposter, Andrea Jade Colomb had a dream, and that dream was to open 80s Bar in Cuba with a neon pink sign of a Flamingo and drink Caprianas all day. Sadly for her that dream didn’t come true, but lucky for us at AJCollective the next dream did…</p>
+				<p class="contact__txt">AJCollective is the new brandchild of Andrea, which ‘like Prince’ was formally known as AJColomb. A creative hub of highly talented individuals that provide visual and written treatments for commercials, music videos and film pitches. </p>
+				<p class="contact__txt">Originally founded in 2007, the company started out as a one man band, with Andrea (of RSA training) crafting treatments from a small studio in Hackney.</p>
+				<p class="contact__txt">After 7 years in the business and realising the demand, Andrea went to the look out for other likeminded souls and found Kat (Treatment Writer), Tegan (Creative Researcher), Jody (Creative Reseacher) and Michael (Creative Researcher) who together began to build the foundations of what is now AJCollective.</p>
 			</div>
 			<div class="cell-11 prepend-2">
-				<h2 class="heading-2 trailer-25">Contact</h2>
+				<p class="contact__txt">Over the past three years the company has rocketed in terms of development, and can now boast not only an arsenal of imagery, but also the experience of working and learning from some of the most influential Directors in the industry, Guy Ritchie, Johan Renck, Johnny Green, Martin Werner, Hamish Rothwell, Sophie Muller, Pete Riski…. Dougal Wilson to name only a few.</p>
+				<p class="contact__txt">This experience, along with our long established relationships and constant support from the best Production Houses and Advertising Agencies around, has been the catalyst for our expansion.</p>
+				<p class="contact__txt">From the humble beginnings of small studio in E8, to a team which now includes a fresh new breed of Creative Researchers, Treatment Writers, Illustrators and Music Technicians based all over Europe. We would like to introduce you to AJCollective, a lively bunch of film nerds with a encyclopaedic knowledge of advertising and photography that put together, with librarians brains and artists hands what’s in the Director’s head and then some...</p>
+			</div>
+		</div>
+		<div class="grid grid--overflow">
+			<div class="prepend-4">
+				<h2 class="heading-2 leader-60 trailer-25">Contact</h2>
+			</div>
+			<div class="cell-11 prepend-4">
 				<div class="contact__bloc">
-					<span class="contact__label">Treatment writing</span>
+					<span class="contact__label">Availability</span>
 					<p class="contact__txt">
 						<span class="contact__txt__label">Katie</span>
-						<a class="contact__txt__label" href="mailto:katie@ajcolomb.com">katie@ajcolomb.com</a> 
+						<a class="contact__txt__label" href="mailto:katie@ajcollective.com">katie@ajcollective.com</a> 
 						<span class="contact__txt__label">0039 (0) 667 303 221</span>
 					</p>
 				</div>
+			</div>
+			<div class="cell-11 prepend-2">
 				<div class="contact__bloc">
-					<span class="contact__label">All other inquieries + availability</span>
+					<span class="contact__label">All other inquires &amp; general chitchat</span>
 					<p class="contact__txt">
 						<span class="contact__txt__label">Andrea</span>
-						<a class="contact__txt__label" href="mailto:andrea@ajcolomb.com">andrea@ajcolomb.com</a>
-						<span class="contact__txt__label">0044 (0) 787 403 6993</span>
+						<a class="contact__txt__label" href="mailto:andrea@ajcollective.com">andrea@ajcollective.com</a>
+						<span class="contact__txt__label">0044 (0) 787403 6993 // 0045 (0) 23 26 26 48</span>
 					</p>
 				</div>
 			</div>
