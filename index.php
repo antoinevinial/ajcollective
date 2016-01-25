062f897fff9b957becf71a8390bd9b5335283411
Antoine Vinial <antoine.vinial@gmail.com>
2016-01-26 00:35:05 +0100

    Change intro panel.

--- index.php
+++ index.php
@@ -28,38 +28,11 @@
 
 			<section class="panel panel--home js-panel is-active" id="home">
 				<div class="panel__container">
-					<ul class="slideshow js-slideshow">
-						<li class="slideshow__item js-slideshow-item" data-color="#6f1527">
-							<img class="slideshow__img" src="resources/slideshow/1.jpg"/>
-						</li>
-						<li class="slideshow__item js-slideshow-item" data-color="#1b2b38">
-							<img class="slideshow__img" src="resources/slideshow/2.jpg"/>
-						</li>
-						<li class="slideshow__item js-slideshow-item" data-color="#859481">
-							<img class="slideshow__img" src="resources/slideshow/3.jpg"/>
-						</li>
-						<li class="slideshow__item js-slideshow-item" data-color="#21515b">
-							<img class="slideshow__img" src="resources/slideshow/4.jpg"/>
-						</li>
-						<li class="slideshow__item js-slideshow-item" data-color="#6a5a4a">
-							<img class="slideshow__img" src="resources/slideshow/5.jpg"/>
-						</li>
-						<li class="slideshow__item js-slideshow-item" data-color="#4d4d4d">
-							<img class="slideshow__img" src="resources/slideshow/6.jpg"/>
-						</li>
-						<li class="slideshow__item js-slideshow-item" data-color="#356356">
-							<img class="slideshow__img" src="resources/slideshow/7.jpg"/>
-						</li>
-						<li class="slideshow__item js-slideshow-item" data-color="#ffdb23">
-							<img class="slideshow__img" src="resources/slideshow/8.jpg"/>
-						</li>
-						<li class="slideshow__item js-slideshow-item" data-color="#ae4c59">
-							<img class="slideshow__img" src="resources/slideshow/9.jpg"/>
-						</li>
-						<li class="slideshow__item js-slideshow-item" data-color="#d3be85">
-							<img class="slideshow__img" src="resources/slideshow/10.jpg"/>
-						</li>
-					</ul>
+					<div class="grid">
+						<div class="cell cell-18 prepend-7">
+							<h2 class="heading-2 intro">A.J Collective lorem ipsum dolor sit amet, consectetur adipisicing elit labore.</h2>
+						</div>
+					</div>
 					<div class="panel__pager">
 						<div class="panel__btn panel__btn--top">
 							<h1 class="panel__btn__label panel__btn__label--lrg">AJCollective</h1>
---
+++ partials/_slideshow.php
@@ -0,0 +1,32 @@
+<ul class="slideshow js-slideshow">
+	<li class="slideshow__item js-slideshow-item" data-color="#6f1527">
+		<img class="slideshow__img" src="resources/slideshow/1.jpg"/>
+	</li>
+	<li class="slideshow__item js-slideshow-item" data-color="#1b2b38">
+		<img class="slideshow__img" src="resources/slideshow/2.jpg"/>
+	</li>
+	<li class="slideshow__item js-slideshow-item" data-color="#859481">
+		<img class="slideshow__img" src="resources/slideshow/3.jpg"/>
+	</li>
+	<li class="slideshow__item js-slideshow-item" data-color="#21515b">
+		<img class="slideshow__img" src="resources/slideshow/4.jpg"/>
+	</li>
+	<li class="slideshow__item js-slideshow-item" data-color="#6a5a4a">
+		<img class="slideshow__img" src="resources/slideshow/5.jpg"/>
+	</li>
+	<li class="slideshow__item js-slideshow-item" data-color="#4d4d4d">
+		<img class="slideshow__img" src="resources/slideshow/6.jpg"/>
+	</li>
+	<li class="slideshow__item js-slideshow-item" data-color="#356356">
+		<img class="slideshow__img" src="resources/slideshow/7.jpg"/>
+	</li>
+	<li class="slideshow__item js-slideshow-item" data-color="#ffdb23">
+		<img class="slideshow__img" src="resources/slideshow/8.jpg"/>
+	</li>
+	<li class="slideshow__item js-slideshow-item" data-color="#ae4c59">
+		<img class="slideshow__img" src="resources/slideshow/9.jpg"/>
+	</li>
+	<li class="slideshow__item js-slideshow-item" data-color="#d3be85">
+		<img class="slideshow__img" src="resources/slideshow/10.jpg"/>
+	</li>
+</ul>
